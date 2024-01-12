27ca086fccaef29aa54d85a68f626318079208b7
John <johnnkonge2020@gmail.com>
2024-01-12 18:48:54 +0300

    fetching from our backend using movies API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
-
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(()=> {
+    fetch("/movies")
+      .then((response) => response.json())
+      .then((movies) => console.log(movies));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Welcome to my Fullstack App!
-        </p>
-        
-       
-      </header>
+    <div>
+      <h1>Movie List</h1>
+      <p>Chech console for the movie list!</p>
     </div>
-  );
+  )
+  
 }
 
 export default App;
